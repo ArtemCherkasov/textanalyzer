7fb60b65f1b10dd17c977df5aaae2ebaf15286af
ARTEM CHERKASOV <artemcherkasov@Air-ARTEM.lan>
2015-12-28 22:27:37 +0300

    add lexicons handle header and source files

---
+++ Headers/LexiconHandle.h
@@ -0,0 +1,15 @@
+/*
+ * LexiconHandle.h
+ *
+ *  Created on: 28 дек. 2015 г.
+ *      Author: artemcherkasov
+ */
+
+#ifndef HEADERS_LEXICONHANDLE_H_
+#define HEADERS_LEXICONHANDLE_H_
+
+class LexiconHandle {
+
+};
+
+#endif /* HEADERS_LEXICONHANDLE_H_ */
---
+++ Source/LexiconHandle.cpp
@@ -0,0 +1,8 @@
+/*
+ * LexiconHandle.cpp
+ *
+ *  Created on: 28 дек. 2015 г.
+ *      Author: artemcherkasov
+ */
+
+#include "../Headers/LexiconHandle.h"
